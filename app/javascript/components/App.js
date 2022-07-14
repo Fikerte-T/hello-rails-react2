6115a4a3aa3a92b4a6682a57515a60628c11b00c
Fikerte-T <tofikertesfaye@gmail.com>
2022-07-14 04:12:58 +0300

    Setup action, reducer and store

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,17 +1,20 @@
 import React from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import PropTypes from "prop-types"
+import {Provider} from 'react-redux'
+import Greeting from './Greeting'
+import store from "../configureStore";
+
 class App extends React.Component {
   render () {
     return (
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={"Home"}/>
-          <Route path="/greetings" element={ <Greeting/>}/>
-
-        </Routes>
-      
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={"Home"}/>
+            <Route path="/greetings" element={ <Greeting/>}/>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
